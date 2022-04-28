b1b9fbf93f449655654d803bf5c3f1be73213105
marumuji <marumuji@gmail.com>
2022-04-29 00:53:40 +0900

    Add table at ProductPage

--- react-material-ui-sample/src/components/pages/ProductPage.tsx
+++ react-material-ui-sample/src/components/pages/ProductPage.tsx
@@ -1,10 +1,72 @@
 import React from 'react';
 import GenericTemplate from '../templates/GenericTemplate';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const createData = (
+  name: string,
+  category: string,
+  weight: number,
+  price: number
+) => {
+  return { name, category, weight, price};
+};
+
+const rows = [
+  createData("チョコーレート", "お菓子", 100, 120),
+  createData("ケーキ", "お菓子", 400, 480),
+  createData("りんご", "フルーツ", 500, 360),
+  createData("バナナ", "フルーツ", 200, 300),
+  createData("みかん", "フルーツ", 250, 180),
+];
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  header: {
+    backgroundColor: "DarkBlue",
+  },
+  headerText: {
+    color: "White",
+  }
+});
 
 const ProductPage: React.FC = () => {
+const classes = useStyles();
+
   return (
     <GenericTemplate title="商品ページ">
-      <>商品ページ内容</> 
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead className={classes.header}>
+            <TableRow>
+              <TableCell className={classes.headerText}>商品名</TableCell>
+              <TableCell className={classes.headerText} align="right">カテゴリー</TableCell>
+              <TableCell className={classes.headerText} align="right">重量(g)</TableCell>
+              <TableCell className={classes.headerText} align="right">価格(円)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.category}</TableCell>
+                <TableCell align="right">{row.weight}</TableCell>
+                <TableCell align="right">{row.price}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </GenericTemplate>
   );
 };
