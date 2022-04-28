8ffc0c1a14c79e9c15c37c54c56b094dbc4e3228
marumuji <marumuji@gmail.com>
2022-04-28 22:38:34 +0900

    Replace files

--- react-material-ui-sample/src/App.tsx
+++ react-material-ui-sample/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import ProductPage from './components/ProductPage';
-import HomePage from './components/HomePage';
+import ProductPage from './components/pages/ProductPage';
+import HomePage from './components/pages/HomePage';
 
 const App: React.FC = () => {
   return (
---
+++ react-material-ui-sample/src/components/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GenericTemplate from '../templates/GenericTemplate';
+                                         
+
+const HomePage: React.FC = () => {
+  return( 
+    <GenericTemplate title="トップページ">
+      <>トップページ</>
+    </GenericTemplate>
+  );
+};
+
+export default HomePage;
